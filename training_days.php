cda4f58811fd6ae2bc9d4c8563de0897ddedac98
Valentine <valentyn.bondar@smile-ukraine.com>
2020-02-21 01:32:56 +0200

    what magic numbers are

--- training_days.php
+++ training_days.php
@@ -1,62 +1,15 @@
 <?php
 /*
- * Profiling custom and internal functions.
+ * Good code examples.
  * Target: drupal training days.
  * Author: vrs11.
  */
 
 /**
- * Profiling code execution time.
+ * Magic numbers.
  */
-function profile($message, callable $func) {
-    $start = microtime(true); //start time of execution in microseconds
-    $func(); //user func to profile
-    $end = microtime(true); //end time of execution in microseconds
-    echo $message, $end-$start, PHP_EOL;
-}
+// What the heck is 86400 for?
+addExpireAt(86400);
 
-$string = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam rhoncus mauris at nisl molestie, 
-           vel viverra elit elementum. Donec nec lobortis purus. Integer auctor nulla nisl, vitae sodales 
-           dui lacinia quis. Nulla id massa nunc. Proin venenatis nisl sed tellus aliquet gravida. Nunc 
-           sollicitudin id augue sit amet commodo. In hac habitasse platea dictumst. Nulla in tellus dolor. 
-           Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Curabitur 
-           non fermentum urna. Suspendisse molestie lacus eget pretium porttitor
-           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam rhoncus mauris at nisl molestie, 
-           vel viverra elit elementum. Donec nec lobortis purus. Integer auctor nulla nisl, vitae sodales 
-           dui lacinia quis. Nulla id massa nunc. Proin venenatis nisl sed tellus aliquet gravida. Nunc 
-           sollicitudin id augue sit amet commodo. In hac habitasse platea dictumst. Nulla in tellus dolor. 
-           Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Curabitur 
-           non fermentum urna. Suspendisse molestie lacus eget pretium porttitor
-           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam rhoncus mauris at nisl molestie, 
-           vel viverra elit elementum. Donec nec lobortis purus. Integer auctor nulla nisl, vitae sodales 
-           dui lacinia quis. Nulla id massa nunc. Proin venenatis nisl sed tellus aliquet gravida. Nunc 
-           sollicitudin id augue sit amet commodo. In hac habitasse platea dictumst. Nulla in tellus dolor. 
-           Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Curabitur 
-           non fermentum urna. Suspendisse molestie lacus eget pretium porttitor
-           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam rhoncus mauris at nisl molestie, 
-           vel viverra elit elementum. Donec nec lobortis purus. Integer auctor nulla nisl, vitae sodales 
-           dui lacinia quis. Nulla id massa nunc. Proin venenatis nisl sed tellus aliquet gravida. Nunc 
-           sollicitudin id augue sit amet commodo. In hac habitasse platea dictumst. Nulla in tellus dolor. 
-           Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Curabitur 
-           non fermentum urna. Suspendisse molestie lacus eget pretium porttitor';
-/**
- * String reverse for a test.
- * Custom reverse function.
- */
-profile('custom string reverse function: ', function() use (&$string)
-{
-    $tmp = '';
-    for ($i = strlen($string) - 1; $i >= 0; $i--) {
-        $tmp .= $string[$i];
-    }
-    $result = $tmp;
-});
-
-/**
- * String reverse for a test.
- * PHP way reverse function.
- */
-profile('PHP strrev function: ', function() use (&$string)
-{
-    $result = strrev($string);
-});
+const SECONDS_IN_A_DAY = 86400;
+addExpireAt(DateGlobal::SECONDS_IN_A_DAY);
