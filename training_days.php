79dc47a9e25fc20cd2caf1ea7b001e4c76efb54e
Valentine <valentyn.bondar@smile-ukraine.com>
2020-02-21 00:53:57 +0200

    just a function

--- training_days.php
+++ training_days.php
@@ -1,13 +1,18 @@
 <?php
-function funkcia_11($mega) {
-if (func_lkzxkf_128($mega) == 0)
-return [];
-$a = $mega[0];
-$b = $c = [];
-for ($i = 1; $i < func_lkzxkf_128($mega); $i++) {
-if ($mega[$i] <$a)
-$b[] = $mega[$i];
-else
-$c[] = $mega[$i];}
-return chegoto_sdelat(funkcia_11($b), [$a], funkcia_11($c));
+
+function quicksort($array) {
+    if (($cnt = count($array)) == 0)
+        return [];
+
+    $pivot_element = $array[0];
+    $left_element = $right_element = [];
+
+    for ($i = 1; $i < $cnt; $i++) {
+        if ($array[$i] < $pivot_element)
+            $left_element[] = $array[$i];
+        else
+            $right_element[] = $array[$i];
+    }
+
+    return array_merge(quicksort($left_element), [$pivot_element], quicksort($right_element));
 }
