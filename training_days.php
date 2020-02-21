49ef48ee2255942ca9f45f1539e9299f75b79d8e
Valentine <valentyn.bondar@smile-ukraine.com>
2020-02-21 02:23:20 +0200

    early return pattern

--- training_days.php
+++ training_days.php
@@ -6,15 +6,47 @@
  */
 
 /**
- * Functions in cycles.
+ * Early return and cognitive complexity.
  */
-//Bad Practice:
-for ($i = 0, $i <= count($array);  $i++) {
-//statements
+
+//not good example
+function some_function() {
+    if ($this->configFactory->get('some config')->get('some value')) {
+        $unsubscribe_nid = $this->configFactory->get('some config')->get('some nid');
+        $unsubscribe = $this->entityTypeManager->getStorage('node')->load($unsubscribe_nid);
+        if (!empty($unsubscribe)) {
+            $node = $unsubscribe;
+
+            if ($unsubscribe->hasTranslation($langcode)) {
+                $unsubscribe = $unsubscribe->getTranslation($langcode);
+                return [
+                    'body' => $this->entityTypeManager->getViewBuilder('node')->view($node),
+                ];
+            }
+        }
+    }
+
+    if (empty($node)) {
+        $routeName = '<front>';
+        $routeParameters = [];
+
+        return $this->redirect($routeName, $routeParameters);
+    }
 }
 
-//Good Practice:
-$count = count($array);
-for ($i = 0; $i < $count;  $i++) {
-//statements
+//refactoring
+CONST FRONT_ROUT_NAME = '<front>';
+CONST FRONT_ROUT_PARAMS = [];
+
+function some_function() {
+    if (
+        empty($unsubscribe_nid = $this->configFactory->get('some config')->get('some nid')) || //check if there is some nid value
+        empty($unsubscribe = $this->entityTypeManager->getStorage('node')->load($unsubscribe_nid)) || //check if there is a node with this id
+        $unsubscribe->hasTranslation($langcode) == FALSE //check if node has a translation
+    ) {
+        return $this->redirect(FRONT_ROUT_NAME, FRONT_ROUT_PARAMS);
+    }
+
+    $unsubscribe = $unsubscribe->getTranslation($langcode);
+    return ['body' => $this->entityTypeManager->getViewBuilder('node')->view($unsubscribe)];
 }
