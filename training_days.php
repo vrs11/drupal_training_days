9fb2c7108b4b74a4cd4e488c32ee3399d264cc91
vBondar <valentyn.bondar@smile-ukraine.com>
2020-02-22 14:57:17 +0200

    code example 1

--- training_days.php
+++ training_days.php
@@ -1,56 +1,37 @@
 <?php
-/*
- * Profiling custom and internal functions.
- * Target: drupal training days.
- * Author: vrs11.
- */
 
 /**
- * Profiling code execution time.
+ * Implemented hook_preprocess_node().
  */
-function profile($message, callable $func) {
-    $start = microtime(true); //start time of execution in microseconds
-    $func(); //user func to profile
-    $end = microtime(true); //end time of execution in microseconds
-    echo $message, $end-$start, PHP_EOL;
+function pnt_front_preprocess_node__event__homepage_widget_event_teaser(&$variables, $hook) {
+    $node = $variables['node'];
+
+    $floor = \Drupal::service('entity_type.manager')->getStorage('paragraph')->loadRevision($node->field_event_floor->value);
+    $variables['floor'] = $floor->field_floor->value;
+    $variables['universe'] = $floor->field_floor_description->value;
+
+    $from_date = strtotime($node->field_dates->value);
+    $to_date = strtotime($node->field_dates->end_value);
+
+    $dateFormatter = \Drupal::service('date.formatter');
+    $variables['from_date'] = $dateFormatter->format($from_date, 'custom', 'd') . ' ' . t($dateFormatter->format($from_date, 'custom', 'F'));
+    $variables['to_date'] = $dateFormatter->format($to_date, 'custom', 'd') . ' ' . t($dateFormatter->format($to_date, 'custom', 'F'));
+
+    $from_date = $dateFormatter->format($from_date, 'custom', 'Ymd');
+    $to_date = $dateFormatter->format($to_date, 'custom', 'Ymd');
+
+    $current_shop_id = \Drupal::service('pnt_common.floors_with_description_helper')->getStoreByFloorParagraphRevisionId($node->field_event_floor->value);
+    $place_string = '';
+    if (!empty($current_shop_id)) {
+        $current_shop = \Drupal::service('entity_type.manager')
+            ->getStorage('node')
+            ->load(reset($current_shop_id));
+        if ($current_shop && $current_shop instanceof NodeInterface) {
+            $place_string = $current_shop->getTitle();
+        }
+    }
+
+    $description = preg_replace('/\s\s+/', '', strip_tags($node->field_description->value));
+    $variables['google_event_string'] = "https://www.google.com/calendar/render?action=TEMPLATE&text={$node->getTitle()}&dates=$from_date/$to_date&details=$description&location=$place_string&sf=true&output=xml";
+
 }
-
-$numbers = range(0, 10000000, 1); //generating test data
-shuffle($numbers); //randomize an array
-
-/**
- * Sorting array for a test.
- * Custom function.
- */
-$sorted =[];
-profile('test 1: ', function() use (&$numbers, &$sorted)
-{
-    $cnt = count($numbers);
-    for ($i = 0; $i < $cnt; $i++) {
-        $sorted[$i] = $numbers[$i];
-    }
-});
-
-/**
- * Sorting array for a test.
- * PHP sort function.
- */
-$sorted =[];
-profile('test 2: ', function() use (&$numbers, &$sorted)
-{
-    foreach ($numbers as $number) {
-        $number = 0;
-    }
-});
-
-/**
- * Sorting array for a test.
- * PHP sort function.
- */
-$sorted =[];
-profile('test 3: ', function() use (&$numbers, &$sorted)
-{
-    foreach ($numbers as &$number) {
-        $number = 0;
-    }
-});
