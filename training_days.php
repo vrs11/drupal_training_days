f3730367ec924b3b6b329f5176074f6e6670d0ea
vBondar <valentyn.bondar@smile-ukraine.com>
2020-02-22 16:20:10 +0200

    example 3

--- training_days.php
+++ training_days.php
@@ -1,39 +1,35 @@
 <?php
-function pnt_front_preprocess_node__service_landing__full(&$variables, $hook) {
-    $node = $variables['node'];
-    $current_service_tid = $node->field_service_type->target_id;
-    if (!empty($current_service_tid)) {
-        $term_storage = \Drupal::service('entity_type.manager')
-            ->getStorage('taxonomy_term');
-        $children = $term_storage->loadChildren($current_service_tid, 'service_type');
-        if (!empty($children)) {
-            $view_builder = \Drupal::entityTypeManager()->getViewBuilder('node');
-            $parameters = \Drupal::request()->query->all();
-            $variables['active_filter'] = $parameters['service_filter'];
-            foreach ($children as $tid => $child) {
-                if ($child->hasTranslation($variables['current_language'])) {
-                    $child = $child->getTranslation($variables['current_language']);
-                }
-                $variables['filters'][$tid] = $child->getName();
-                asort($variables['filters']);
-                if (!empty($parameters['service_filter']) && $parameters['service_filter'] !== 'all' && $parameters['service_filter'] != $tid) {
-                    continue;
-                }
-                $related_service_details[$tid] = \Drupal::service('printemps_sl.related_services.loader')
-                    ->loadServiceDetailsPagesByServiceType($tid);
-                if (empty($related_service_details[$tid])) {
-                    continue;
-                }
+function pnt_front_preprocess_widget__event_display__default(&$variables, $hook) {
+    $variables['#cache']['tags'][] = 'node:event';
 
-                $variables['related_service_details'][$tid] = $view_builder->viewMultiple($related_service_details[$tid], 'teaser_on_service_landing');
-            }
+    $langcode = \Drupal::languageManager()->getCurrentLanguage()->getId();
 
-            array_push($variables['#cache']['contexts'], 'url.query_args:service_filter');
-
-            array_push($variables['#cache']['tags'],
-                'node:service_details',
-                'node:service_landing'
-            );
-        }
+    $node = \Drupal::routeMatch()->getParameter('node');
+    if ($node instanceof \Drupal\node\NodeInterface) {
+        $store_nid = $node->id();
     }
+    else {
+        return;
+    }
+
+    $all_store_floors = \Drupal::service('pnt_common.floors_with_description_helper')->getAllDescriptionRevisionsByStoreId($store_nid);
+
+    $now = new DrupalDateTime('now');
+    $events_query = \Drupal::entityTypeManager()->getStorage('node')->getQuery();
+
+    $events = $events_query->condition('type', 'event')
+        ->condition('status', NodeInterface::PUBLISHED)
+        ->condition('field_event_floor', $all_store_floors, 'IN')
+        ->condition('field_dates.end_value', $now->format(DateTimeItemInterface::DATETIME_STORAGE_FORMAT), '>')
+        ->execute();
+
+    if (!empty($events)) {
+        $events = \Drupal::entityTypeManager()
+            ->getStorage('node')
+            ->loadMultiple($events);
+        $variables['events'] = \Drupal::entityTypeManager()
+            ->getViewBuilder('node')
+            ->viewMultiple($events, 'store_widget_event_teaser');
+    }
+
 }
